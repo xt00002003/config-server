a0486b7e0394cc377479d6e91530081d60a922ef
dark <xt00002003@163.com>
2017-07-18 18:04:31 +0800

    添加配置信息

--- src/main/java/com/dark/ConfigApplication.java
+++ src/main/java/com/dark/ConfigApplication.java
@@ -1,14 +1,17 @@
 package com.dark;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 
 
+@Configuration
+@EnableAutoConfiguration
+@RestController
 @EnableConfigServer
-@SpringBootApplication
 public class ConfigApplication {
 
 	public static void main(String[] args) {
